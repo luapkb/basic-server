828491d01fdbdf8185f26890457f248df5b5e770
paul bennett <luapkb@gmail.com>
2019-10-18 17:24:06 -0700

    deploypush:

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {res.send('hello paul')});
+app.use(express.static('./public'));
+
+app.get('/hello', (req, res) => {res.send('hello paul')});
+app.get('/hello/greenterrd', (req, res) => {res.send ('blue')});
+app.get('/speedboat', (req, res) => {res.send('a purple goose')});
+
+
+app.use('*', (request, response) => response.send('the route is bad'));
+
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT})!`));
+
 
